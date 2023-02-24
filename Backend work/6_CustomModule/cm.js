a0b49336535fd6e7713f140467147a842c88b44d
Sakshi <iamsakshiingole@gmail.com>
2023-02-24 17:56:10 -0500

    Module i

---
+++ Backend work/6_CustomModule/cm.js
@@ -0,0 +1,7 @@
+//const average=require('./module');
+//console.log(average([3,4]));
+
+const mod=require('./module');
+console.log(mod.avg([3,4]));
+console.log(mod.name);
+console.log("This is cm.js");
---
+++ Backend work/7_npm/npm.js
@@ -0,0 +1,17 @@
+console.log("This is ---");
+
+//"slugify":"^1.3.5"        //major.minor.patch(bug fixes)
+//^ meant: downloads exactly the same version
+//~ meant: downloads the new acceptable patch
+
+//npm init,
+//npm install slugify or npm install slugify@1.3.5
+
+//nodemon is an utility which is used to restart a server
+//npm install nodemon --save-dev(for a project only) or npm install nodemon --global(for node.js folder)
+
+//npm i browserify
+
+//npm i: installs the latest version of the modules
+
+//If node module folder is deleted then write npm insall, all the node modules will be installed
