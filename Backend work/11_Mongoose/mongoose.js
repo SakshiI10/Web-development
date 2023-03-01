7d7a940408d9cc2bacbd4cd384c887940e68364c
Sakshi <iamsakshiingole@gmail.com>
2023-03-01 16:23:42 -0500

    Mongoose

--- Backend work/11_Mongoose/mongoose.js
+++ Backend work/11_Mongoose/mongoose.js
@@ -2,7 +2,29 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/smi', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-    console.log("Connected");
+//db.on('error', console.error.bind(console, //'connection error:'));
+//db.once('open', function(){
+//    console.log("Connected, this has been run //after connection establishment.");
+//});
+
+var kittySchema = new mongoose.Schema({
+    name: String
 });
+
+//kittySchema.methods.speak = function speak() {
+//    var greeting = "My name is " + this.name
+//    console.log(greeting);
+//};
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'silence' });
+console.log(silence.name); // 'Silence'
+//var silence2 = new Kitten({ name: 'Silence2' });
+//console.log(silence2.name); // 'Silence2'
+
+silence.save(function (err, silence) {
+    if (err) return console.error(err);
+    silence.speak();
+  });
+
