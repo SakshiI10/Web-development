9d48c54d8ac1924ba397632e1dd1a4efb2a551bb
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-20 23:18:21 +0530

    file name changed

---
+++ Frontend work/42_Inspect_On_Any_WP.js
@@ -0,0 +1,9 @@
+// Try inpect on any web page and find any id in the code.
+
+// 1. document.getElementById("paste_id_here")
+
+// 2. document.getElementById("paste_id_here").click(): the clilck will be happened programmatically
+
+// 3. id_name: gives you the id 
+
+// 4. id_name.innerHTML = "type_whatever_you_want": you can change the element appearance directly on the webpage.
--- Frontend work/43_Inspect_On_Any_WP.js
+++
@@ -1,4 +0,0 @@
-// Try inpect on any web page and find any id in the code.
-
-// 1. document.getElementById("paste_id_here")
-// 2. document.getElementById("paste_id_here").click(): the clilck will be happened programmatically
