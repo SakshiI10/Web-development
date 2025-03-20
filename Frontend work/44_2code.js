e3f4e87ff8b2116fc2484591d80289eeb4943496
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-20 23:45:12 +0530

    file name changed

--- Frontend work/44_2code.js
+++ Frontend work/44_2code.js
@@ -1,12 +1,50 @@
+//Variables
 var a = 78;
 var b = "Sakshi";
 c = 34.55;
 console.log(c);
 
+// Data Types
+// 1. Primitive DT
+// a. Numbers
+var num1 = 456
+var num2 = 56.76
+
+// b. Strings
+var str1 = "This is a string"
+var str2 = "This is also a string"
+
+// c. Booleans
+var b1=true
+var b2=false
+console.log(b1, b2)
+
+// d. undefined
+var und
+console.log(und)
+
+// e. null
+var n=null
+console.log(n)
+
+// f. symbol
+
+// 2. Reference DT
+// a. Array
+var arr=[1, 2.0, "three", undefined, null]
+console.log(arr)
+console.log(arr[2])
+
+// b. Objects
+var marks = {
+    abc: 96,
+    def: 85,
+    ghi: 74
+}
+
 // Operators in JavaScript
 // Operand - entities on which operators operate
 // In 3 + 4 '+' is the operator and 3,4 are operands
-
 // 1. unary operator - It has single operand (x = -x)
 // Examples of unary operator
 c = -c;
@@ -20,7 +58,7 @@ console.log(c);
 var num1 = 2;
 var num2 = 9;
 
-// Arithmetic operators in action in JavaScript
+// 3. Arithmetic operators in action in JavaScript
 // console.log("The value of num1 + num2 is " + (num1 + num2));
 // console.log("The value of num1 - num2 is " + (num1 - num2));
 //console.log("The value of num1 * num2 is " + (num1 * num2));
@@ -28,4 +66,18 @@ var num2 = 9;
 // console.log("The value of num1 ** num2 is " + (num1 ** num2));
 // console.log("The value of num1++ is " + (num1++));
 // console.log("The value of ++num1 is " + (++num1));
-console.log("The value of num1-- is " + (num1--));
+// console.log("The value of num1-- is " + (num1--));
+
+// 4. Comparison Operators
+var x=34
+var y=56
+// console.log(x==y)
+// console.log(x>=y)
+// console.log(x<=y)
+// console.log(x>y)
+// console.log(x<y)
+
+// 5. Logical Operator
+console.log(true && true)
+console.log(true || true)
+console.log(!true)
