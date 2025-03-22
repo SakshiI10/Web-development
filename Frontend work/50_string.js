687567889735b29eb8f23295f3d92d597478bb03
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-22 12:26:46 +0530

    file edited

---
+++ Frontend work/50_string.js
@@ -0,0 +1,6 @@
+let str1="JS is a nice language"
+console.log(str1.length)
+console.log(str1.indexOf("nice"))
+console.log(str1.lastIndexOf("nice"))
+console.log(str1.slice(0, 4))
+console.log(str1.replace("JS", "React"))
