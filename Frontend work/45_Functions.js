3acb8c48b4ca465fedc7f8f6880656c954dd803f
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-22 11:17:57 +0530

    file edited

--- Frontend work/45_Functions.js
+++ Frontend work/45_Functions.js
@@ -1,30 +1,17 @@
-function greet(name, greetText = "Greetings from JavaScript") {
-    let name1 = "Name1";
-    console.log(greetText + " " + name);
-    console.log(name + " is a good boy");
-}
-
 function sum(a, b, c) {
     let d = a + b + c;
     return d;
-    // This line will never execute (Unreachable code)
-    // console.log("Function is returned");
 }
+s1=sum(1,2,3)
+s2=sum(4,5,6)
+console.log(s1, s2)
 
-let name = "abc";
-let name1 = "def";
-let name2 = "ghi";
-let name3 = "jkl";
+
+function greet(name="Sakshi", greetText = "Greetings from JavaScript,") {
+    console.log(greetText + " " + name);
+    console.log(name + " is a good person");
+}
+let name1 = "abc";  
 let greetText1 = "Good Morning";
-greet(name, greetText1);
-greet(name1, greetText1);
-greet(name2, greetText1);
-// let returnVal = greet(name3);
-// console.log(returnVal)
-
-let returnVal = sum(1, 2, 3);
-console.log(returnVal)
-// console.log(name + " is a good boy");
-// console.log(name1 + " is a good boy");
-// console.log(name2 + " is a good boy");
-// console.log(name3 + " is a good boy"); 
+greet(name1, greetText1);  
+greet()
