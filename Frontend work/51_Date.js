e137513d65c4c32944ecdd308bc77b718f101893
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-23 09:54:19 +0530

    file edited

---
+++ Frontend work/51_Date.js
@@ -0,0 +1,7 @@
+let mydate=new Date()
+console.log(mydate)
+console.log(mydate.getTime())   //Time in seconds
+console.log(mydate.getFullYear())
+console.log(mydate.getDay())
+console.log(mydate.getHours())
+// Explore new dates on internet
