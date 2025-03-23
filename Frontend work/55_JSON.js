e4bec784427a931a241c1a03b8f31e666838cf56
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-23 18:29:09 +0530

    file edited

---
+++ Frontend work/55_JSON.js
@@ -0,0 +1,11 @@
+// Creating an object
+obj = {name: "abc", length: 1, a: {this: 'tha"t'}}
+// Converting to JSON string
+jso = JSON.stringify(obj)
+console.log(jso)
+console.log(typeof jso)
+
+// Pass a valid JSON string to JSON.parse(), which converts it back into an object.
+// JSON strings must use double quotes (") around property names and string values.
+parsed = JSON.parse('{"name": "abc", "length": 1, "a": {"this": "that"}}')
+console.log(parsed)
