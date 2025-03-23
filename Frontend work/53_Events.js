5379b0b189ce0db12ad6fb1b7d8ed110d3158e41
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-23 11:55:08 +0530

    file edited

---
+++ Frontend work/53_Events.js
@@ -0,0 +1,31 @@
+function clicked(){
+    console.log('The button was clicked')
+}
+
+window.onload = function(){
+    console.log('Document loaded')
+}
+
+firstContainer.addEventListener('click', function(){
+    document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+    console.log('The Container was clicked')
+})
+
+secContainer.addEventListener('mouseover', function(){
+    console.log("Mouse on Container")
+})
+
+// secContainer.addEventListener('mouseout', function(){
+    //     console.log("Mouse out of Container")
+    // })
+    
+    let prevHTML = document.querySelectorAll('.container')[1].innerHTML
+    secContainer.addEventListener('mouseup', function(){
+        document.querySelectorAll('.container')[1].innerHTML=prevHTML
+        console.log("Mouse up when clicked on Container")
+    })
+    
+secContainer.addEventListener('mousedown', function(){
+    document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+    console.log("Mouse down when clicked on Container")
+})
